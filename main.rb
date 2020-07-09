999266f7fc8fdedee6c71c600cca0d19b63d0030
wrakc <carlosveiga1618@gmail.com>
2020-07-09 12:02:32 -0300

    add my_each_with_index method

--- main.rb
+++ main.rb
@@ -4,4 +4,12 @@ module Enumerable
     size.times { |i| yield to_a[i] }
     self
   end
+
+  def my_each_with_index
+    return enum_for(__method__) unless block_given?
+    size.times { |i| yield to_a[i], i }
+    self
+  end
+
+  
 end
