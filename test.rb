bd2288d06449bce1cbae41a4a09a87da15e9d0dd
wrakc <carlosveiga1618@gmail.com>
2020-07-09 12:18:09 -0300

    add my_none method tests and demo

--- test.rb
+++ test.rb
@@ -18,7 +18,7 @@ puts "\n"
 print [1,2,3,4,5].select { |num|  num.even? }
 puts "\n\n\n"
 
-puts "my_all"
+puts "my_all?"
 puts %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
 puts %w[ant bear cat].all? { |word| word.length >= 4 } #=> false
 puts %w[ant bear cat].all?(/t/)                        #=> false
@@ -27,12 +27,23 @@ puts [nil, true, 99].all?                              #=> false
 puts [].all?                                           #=> true 
 puts "\n\n\n"
 
-puts "my_any"
+puts "my_any?"
 puts %w[ant bear cat].any? { |word| word.length >= 3 } #=> true
 puts %w[ant bear cat].any? { |word| word.length >= 4 } #=> true
 puts %w[ant bear cat].any?(/d/)                        #=> false
 puts [nil, true, 99].any?(Integer)                     #=> true
 puts [nil, true, 99].any?                              #=> true
 puts [].any?                                           #=> false
+puts "\n\n\n"
+
+puts "my_none?"
+puts %w{ant bear cat}.none? { |word| word.length == 5 } #=> true
+puts %w{ant bear cat}.none? { |word| word.length >= 4 } #=> false
+puts %w{ant bear cat}.none?(/d/)                        #=> true
+puts [1, 3.14, 42].none?(Float)                         #=> false
+puts [].none?                                           #=> true
+puts [nil].none?                                        #=> true
+puts [nil, false].none?                                 #=> true
+puts [nil, false, true].none?                           #=> false
 puts "\n\n\n"
 
