28a6c389908344e1764c13b751d0447714bd8949
wrakc <carlosveiga1618@gmail.com>
2020-07-09 12:07:55 -0300

    add my_all and its related methods

--- main.rb
+++ main.rb
@@ -18,4 +18,25 @@ module Enumerable
     my_each { |n| array << n if yield n }
     array
   end
+
+  def my_all?(arg = nil)
+    puts "#{__FILE__}:#{__LINE__}: w: block not used" if arg && block_given?
+    return my_all_extra_func(arg) if arg
+
+    if !block_given?
+      size.times { |n| return false if to_a[n] == false || to_a[n].nil? }
+    else
+      my_each { |n| return false unless yield n }
+    end
+    true
+  end
+
+  def my_all_extra_func(arg)
+    if arg.is_a?(Regexp)
+      my_each { |n| (n =~ arg).nil? ? false : 'Error' }
+      true
+    elsif arg
+      my_each { |n| return false unless n.is_a? arg }
+    end
+  end
 end
