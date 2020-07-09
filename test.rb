547da5cf15170ce9575141ecd59984d6720d8f52
wrakc <carlosveiga1618@gmail.com>
2020-07-09 12:04:56 -0300

    add my_each_with_index demo

--- test.rb
+++ test.rb
@@ -4,5 +4,13 @@ puts "my_each"
 puts "\n"
 a = %w[a b c]
 a.each {|x| print x, " -- " }
-puts "\n\n"
+puts "\n\n\n"
+
+puts "my_each_with_index"
+puts "\n"
+hash = Hash.new
+%w(cat dog wombat).each_with_index { |item, index| hash[item] = index }
+puts hash
+puts "\n\n\n"
+
 
