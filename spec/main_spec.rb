cb4d17cb514fd37ff8ecd69570dd447b43aaa893
wrakc <carlosveiga1618@gmail.com>
2020-08-11 12:11:33 -0300

    add iteration over words for my_each test
    add --format documentation to .rspec

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,3 +1,9 @@
-  require './main'
+  require './main.rb'
 
-  
+  describe "#my_each" do
+    it "should return the iteration of an array" do
+      array_words = %w[a b c]
+      result = []
+      expect(array_words.my_each{ |w| result << w }).to eql(result)
+    end  
+  end
